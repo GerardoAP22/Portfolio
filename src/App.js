97d51f6a7e33585fc20ccfa04c701d9cffc33655
GerardoAP22 <garriazapantaleon@colgate.edu>
2023-11-08 18:37:02 -0500

    Adds Skeleton of about projects and contact

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <About />
+      <Projects />
+      <Contact />
     </div>
   );
 }
