67836c7346f1e00c63fdab1ccab72a8bdd8d07d4
GerardoAP22 <garriazapantaleon@colgate.edu>
2023-11-29 23:58:30 -0500

    Adds animated image border for about section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
@@ -9,7 +8,6 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home />
       <About />
       <Projects />
       <Contact />
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { HiArrowNarrowRight } from 'react-icons/hi';
 
 function About() {
   return (
-    <div>
+    <div className='about-container'>
         {/* <img src="" alt="Gerardo_image"></img>
         <p>Hello my name is Gerardo and I am a Software Engineer located in the Bronx, New York. I am committed to helping others achieve excellence who wants to contribute to building a product that will benefit the user. As a camp counselor and trainer, I worked with many different types of people with many different backgrounds and pushed them to try new experiences and achieve their individual goals. Now I want to build digital products that can help people in similar ways. Throughout customer service, training, and mentoring opportunities I learned to communicate with teams, while also being individually dependent on the task at hand. I leverage these experiences as well as my drive to achieve progress to help debug any issues that will allow my team to produce a better product for the consumer. I want to expand my skills throughout the job and contribute as much as I can to the team overall.</p> */}
+    <div className='about-image-container'>
+        <div className='about-image'>
+            <div className='image'>
+                <img src=''/>
+            </div>
+        </div>
+    </div>
     </div>
   )
 }
