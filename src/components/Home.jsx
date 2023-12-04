24c1e1021a601b2c9c3488db5ae32968c4cdc449
GerardoAP22 <garriazapantaleon@colgate.edu>
2023-12-04 14:20:49 -0500

    aligns home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import { HiArrowNarrowRight } from 'react-icons/hi'
+import React from 'react';
+import { HiArrowNarrowRight } from 'react-icons/hi';
+import headshot from '../assets/Photo.jpeg';
+
 
 function Home() {
   return (
     <div className='home-comp'>
+        <div className='home-image-container'>
+        <div className='home-image'>
+          <div className='image'>
+            <img src={headshot} alt="Headshot" />
+          </div>
+        </div>
+      </div>
 
         <div className='home-container'>
             <p className='home-hello-txt'>Hello, my name is</p>
             <h1 className='home-name-txt'>Gerardo Arriaza Pantaleon</h1>
             <h2 className='home-role-txt'>I'm a Full Stack Developer.</h2>
-            <p>I am a full-stack developer who is leaning a bit more on 
+            <p className='home-desc-txt'>I am a full-stack developer who is leaning a bit more on 
                 front-end development. I'm focused on building responsive 
             full-stack web applications.</p>
             <div className='home-button-container'>
