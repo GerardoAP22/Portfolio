e2f6eb3ef0c872ac2760a080e83fecba65dd7972
GerardoAP22 <garriazapantaleon@colgate.edu>
2023-12-04 14:21:38 -0500

    fixes small bug

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,13 +4,6 @@ import headshot from '../assets/Photo.jpeg';
 function About() {
   return (
     <div className='about-container'>
-      <div className='about-image-container'>
-        <div className='about-image'>
-          <div className='image'>
-            <img src={headshot} alt="Headshot" />
-          </div>
-        </div>
-      </div>
       <div className='about-text-container'>
         <p>Hello, my name is Gerardo Arriaza Pantaleon. Nice to meet you.</p>
         <p>I am a Software Engineer located in the Bronx, New York. I am passionate about building excellent software that improves the lives and experiences of users. I am expanding my skills and look forward to using my abilities in helping achieve excellence by building a product that benefits users.</p>
