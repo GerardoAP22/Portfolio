b6cf733ec14d07ad75bc9566f9767e825f9c31e2
GerardoAP22 <garriazapantaleon@colgate.edu>
2023-12-05 01:14:15 -0500

    Aligns p tag in about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,9 +14,8 @@ function About() {
                         <div className='about-hello-container'>
                             <p>Hello, my name is Gerardo. Nice to meet you. Please take a look at my work!</p>
                         </div>
-                        <div>
+                        <div className='about-desc-container'>
                             <p>I am a Software Engineer located in the Bronx, New York. I am passionate about building excellent software that improves the lives and experiences of users. I am expanding my skills and look forward to using my abilities in helping achieve excellence by building a product that benefits users.</p>
-
                         </div>
                     </div>
                 </div>
