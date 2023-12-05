cd2dd82f87e941377267fef94d487a9d457a63be
GerardoAP22 <garriazapantaleon@colgate.edu>
2023-12-05 01:06:56 -0500

    Completes about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
-import headshot from '../assets/Photo.jpeg';
 
 function About() {
-  return (
-    <div className='about-container'>
-      <div className='about-text-container'>
-        <p>Hello, my name is Gerardo Arriaza Pantaleon. Nice to meet you.</p>
-        <p>I am a Software Engineer located in the Bronx, New York. I am passionate about building excellent software that improves the lives and experiences of users. I am expanding my skills and look forward to using my abilities in helping achieve excellence by building a product that benefits users.</p>
-      </div>
-    </div>
-  );
+    return (
+        <div className='about-container'>
+            <div className='about-in-con'>
+                <div className='about-content-container'>
+                    <div className='about-title-container'>
+                        <p className='about-title'>About</p>
+                    </div>
+                    <div></div>
+                    </div>
+                    <div className='about-text-container'>
+                        <div className='about-hello-container'>
+                            <p>Hello, my name is Gerardo. Nice to meet you. Please take a look at my work!</p>
+                        </div>
+                        <div>
+                            <p>I am a Software Engineer located in the Bronx, New York. I am passionate about building excellent software that improves the lives and experiences of users. I am expanding my skills and look forward to using my abilities in helping achieve excellence by building a product that benefits users.</p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+    );
 }
 
 export default About;
